6f8bfde46c48b9172594c482804d0ca1ae5323d9
Nikita Glazunov <2004glaz@mail.ru>
2024-04-06 20:02:06 +0500

    Serialization fixes

--- PulseAndPower.Core/Models/Common/SidEntity.cs
+++ PulseAndPower.Core/Models/Common/SidEntity.cs
@@ -1,8 +1,13 @@
-﻿namespace PulseAndPower.BusinessLogic.Models.Common;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace PulseAndPower.BusinessLogic.Models.Common;
 
 public class SidEntity
 {
+    [BsonRepresentation(BsonType.String)]
     public Guid Id { get; set; }
     
+    [BsonRepresentation(BsonType.String)]
     public Guid UserId { get; set; }
 }
--- PulseAndPower.Core/Models/Common/UserStatus.cs
+++ PulseAndPower.Core/Models/Common/UserStatus.cs
@@ -1,5 +1,8 @@
-﻿namespace PulseAndPower.BusinessLogic.Models.Common;
+﻿using System.Text.Json.Serialization;
 
+namespace PulseAndPower.BusinessLogic.Models.Common;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
 public enum UserStatus
 {
     CodeSent,
